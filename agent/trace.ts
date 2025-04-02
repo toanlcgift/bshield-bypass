933c9c2cd07aeab471aca133b5ce9eef8a471b58
toanlcgift <toanlcgift@gmail.com>
2025-04-02 18:11:27 +0700

    hook mach_port_allocate

--- agent/trace.ts
+++ agent/trace.ts
@@ -10,3 +10,15 @@ Interceptor.attach(runtime_errorFunc, {
         console.log('runtime_errorFunc return:' + retval.readCString());
     }
 });
+
+// kern_return_t __cdecl mach_port_allocate(ipc_space_t task, mach_port_right_t right, mach_port_name_t *name)
+const mach_port_allocateFunc = Module.findExportByName('bshield', '_mach_port_allocate') ?? new NativePointer(0x00);
+
+Interceptor.attach(mach_port_allocateFunc, {
+    onEnter(args) {
+        console.log('mach_port_allocateFunc:');
+    },
+    onLeave(retval) {
+        console.log('mach_port_allocateFunc return:' + retval.readCString());
+    }
+});
